d0b0491dc989597cb103200bf8b98b42df4cbf45
Anna Antonenko <hi@portasynthinca3.me>
2022-05-01 19:29:24 +0300

    feat: LSP support

--- README.md
+++ README.md
@@ -7,6 +7,12 @@
 ![PRs and issues](https://img.shields.io/badge/PRs%20and%20issues-welcome-brightgreen)
 
 # SUS
-Provides SUS syntax highlighting.
+Provides SUS syntax highlighting and errors.
 
 You can read more about the language [here](https://github.com/amogus-api/info).
+
+## Prerequisites
+Install SUSC (used as the language server here) with:
+```
+$ pip3 install "susc>=1.1.1"
+```
---
+++ src/extension.ts
@@ -0,0 +1,42 @@
+import * as ChildProcess from "child_process";
+import { ExtensionContext } from "vscode";
+import { LanguageClient, LanguageClientOptions } from "vscode-languageclient/node";
+import { waitForSocket } from "socket-retry-connect";
+import denodeify from "denodeify";
+
+let client: LanguageClient;
+
+async function startServer(dev: boolean) {
+	if(!dev) {
+		const susc = ChildProcess.exec("susc -si");
+		return susc;
+	}
+
+	const wait = denodeify(waitForSocket) as ({ port: number }) => any;
+	const socket = await wait({ port: 9090 });
+	console.log("connected");
+	return { writer: socket, reader: socket };
+}
+
+export function activate(context: ExtensionContext) {
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: ["sus"],
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		"suslang",
+		"SUS Language Server",
+		() => startServer(false),
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if(!client)
+		return undefined;
+	return client.stop();
+}
